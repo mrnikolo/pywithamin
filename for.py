0345a93114c26d528e39abcc61e3be5ad6b02898
mrarashdev <arashm2710@gmail.com>
2022-08-10 12:39:12 +0430

    toturial about statement for in python

---
+++ for.py
@@ -0,0 +1,31 @@
+# lists = [1 , True, 5 , 'be to che']
+
+
+# for item in lists:
+#     print(item)
+# # **********************************
+
+# a = 'amin'
+# for m in a:
+#     print(m)
+# # ***********************************
+
+# for i in lists:
+#     print(i)
+#     if i == 5:
+#         break
+
+# for amin in lists:
+#     print(amin)
+#     if amin == True:
+#         continue
+
+# for k in lists:
+#     print(k)
+#     if k == 1:
+#         break
+
+
+for i in range(5, 100, 5):
+    print(i)
+
